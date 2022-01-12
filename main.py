a8a2b254e387d3fe116291bd65d49dde63ef92c2
vasyz <vladyar33@gmail.com>
2022-01-13 00:00:42 +0500

    asd

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
 import sys
-from random import randint
-
-from PyQt5 import QtCore
-from PyQt5 import QtGui
-from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtGui import QPainter, QColor
+import sqlite3
+from PyQt5.QtWidgets import QTableWidgetItem
+from PyQt5 import uic
 from PyQt5.QtWidgets import *
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('untitled.ui', self)  # Загружаем дизайн
-        self.pushButton.clicked.connect(self.paint)
-        self.do_paint = False
-
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_yellow_circle(qp)
-            qp.end()
-
-    def paint(self):
-        self.do_paint = True
-        self.repaint()
-
-    def draw_yellow_circle(self, qp):
-        rand_color = randint(1, 255), randint(1, 255), randint(1, 255)
-        rand_size = randint(50, 100)
-        qp.setBrush(QColor(*rand_color))
-        qp.setPen(QtGui.QPen(QColor(*rand_color), 1, QtCore.Qt.SolidLine))
-        qp.drawEllipse(randint(1, 800), randint(1, 600), rand_size, rand_size)
-        self.do_paint = False
+        uic.loadUi('main.ui', self)
+        self.resize(1500, 1000)
+        conor = sqlite3.connect('coffee.sqlite')
+        nor = conor.cursor()
+        result = nor.execute(f'''SELECT * FROM coffee''').fetchall()
+        conor.close()
+        for coffee in result:
+            rowPosition = self.tableWidget.rowCount()
+            self.tableWidget.insertRow(rowPosition)
+            self.tableWidget.setItem(rowPosition, 0, QTableWidgetItem(str(coffee[0])))
+            self.tableWidget.setItem(rowPosition, 1, QTableWidgetItem(str(coffee[1])))
+            self.tableWidget.setItem(rowPosition, 2, QTableWidgetItem(str(coffee[2])))
+            self.tableWidget.setItem(rowPosition, 3, QTableWidgetItem(str(coffee[3])))
+            self.tableWidget.setItem(rowPosition, 4, QTableWidgetItem(str(coffee[4])))
+            self.tableWidget.setItem(rowPosition, 5, QTableWidgetItem(str(coffee[5])))
+            self.tableWidget.setItem(rowPosition, 6, QTableWidgetItem(str(coffee[6])))
 
 
 if __name__ == '__main__':
